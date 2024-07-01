bbe6c4bb7d9e0d6b2bbfe05372bf878afc4deb01
LupoArticoblu <wolf94-@libero.it>
2024-07-01 12:52:20 +0200

    function getAPI()

--- Axios/script.js
+++ Axios/script.js
@@ -9,19 +9,25 @@ createApp({
       numero: '',
     };
   },
-  mounted() {
-    axios.get('https://flynn.boolean.careers/exercises/api/random/int')
-    //risposta DI Callback (usare le arrow function perchè altrimenti Vue va in conflitto utilizzando this come per le funzioni interval)
-    .then((response) => {
-      console.log(response.data);
-      //valorizzo numero con la risposta per stamparla in pagina
-      this.numero = response.data.response;
-    })
-    //nel caso in vui l'indirizzo cambi o ci siano altri errori usiamo catch per riconoscerli e gestirli
-    .catch((error) => {
-      console.log(error.code);
-    })
-  },
   methods: {
-  }
+    //funzione che richiama l'API
+    getApi(){
+      axios.get('https://flynn.boolean.careers/exercises/api/random/int')
+      //risposta DI Callback (usare le arrow function perchè altrimenti Vue va in conflitto utilizzando this come per le funzioni interval)
+      .then((response) => {
+        console.log(response.data);
+        //valorizzo numero con la risposta per stamparla in pagina
+        this.numero = response.data.response;
+      })
+      //nel caso in vui l'indirizzo cambi o ci siano altri errori usiamo catch per riconoscerli e gestirli
+      .catch((error) => {
+        console.log(error.code);
+      })
+
+    }
+  },
+  mounted() {
+    //tenere il mounted pulito ed usarlo solo per richiamare le funzioni
+    this.getApi();
+  },
 }).mount('#app');
