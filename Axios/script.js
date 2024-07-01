adf509892991dfa25ee34573bef2959e3d216e28
LupoArticoblu <wolf94-@libero.it>
2024-07-01 12:45:58 +0200

    il metodo catch

--- Axios/script.js
+++ Axios/script.js
@@ -11,11 +11,15 @@ createApp({
   },
   mounted() {
     axios.get('https://flynn.boolean.careers/exercises/api/random/int')
-    //risposta DI Callback
+    //risposta DI Callback (usare le arrow function perchè altrimenti Vue va in conflitto utilizzando this come per le funzioni interval)
     .then((response) => {
       console.log(response.data);
       //valorizzo numero con la risposta per stamparla in pagina
       this.numero = response.data.response;
+    })
+    //nel caso in vui l'indirizzo cambi o ci siano altri errori usiamo catch per riconoscerli e gestirli
+    .catch((error) => {
+      console.log(error.code);
     })
   },
   methods: {
