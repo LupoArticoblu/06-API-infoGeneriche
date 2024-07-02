595da1f49ed413a1bc34afca1946302e270d7002
LupoArticoblu <wolf94-@libero.it>
2024-07-02 17:34:34 +0200

    isLoading [fixed]

--- Open Library/script.js
+++ Open Library/script.js
@@ -1,31 +1,58 @@
 const { createApp } = Vue;
 
-console.log(axios);
-
 createApp({
   data() {
     return {
       title: 'Open library',
       apiUrl: 'http://openlibrary.org/search.json',
-      author: 'Zlatan Ibrahimovic',
+      author: '',
       books: [],
-      isLoading: false
+      isLoading: false,
+      currentPlaceholder: '',
+      place: [
+        'Manzoni',
+        'Dante',
+        'Tolstoj',
+        'Shakespeare',
+        'Hugo',
+        'Vonnegut',
+      ],
+      pIndex: 0,
+      pInterval: null,
     };
   },
   methods: {
-    //funzione che richiama l'API
-    getApi(){
-      this.isLoading = false;
-      axios.get(this.apiUrl + '?author=' + this.author) 
-      .then((response) => {
-        console.log(response.data.docs);
-        this.books = response.data.docs;
-        this.isLoading = true;
-      })
+    // Funzione che richiama l'API
+    getApi() {
+      this.isLoading = true;
+      axios.get(`${this.apiUrl}?author=${this.author}`)
+        .then((response) => {
+          console.log(response.data.docs);
+          this.books = response.data.docs;
+          this.isLoading = false;
+        })
+        .catch((error) => {
+          console.error(error);
+          this.isLoading = false;
+        });
+    },
+
+    // Funzione che cambia il placeholder
+    changePlaceholder() {
+      this.currentPlaceholder = this.place[this.pIndex];
+      this.pInterval = setInterval(() => {
+        this.pIndex = (this.pIndex + 1) % this.place.length;
+        this.currentPlaceholder = this.place[this.pIndex];
+      }, 2000);
+    },
+    check() {
+      clearInterval(this.pInterval);
     }
   },
   mounted() {
-    
-    this.getApi();
+    this.changePlaceholder();
+  },
+  beforeUnmount() {
+    this.check();
   },
 }).mount('#app');
